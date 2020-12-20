d199dd029ac823f4e44621a1dad7ce9a118f0019
Sebastián Monía <smonia@outlook.com>
2020-12-19 19:40:36 -0700

    Get list of files from server and populate listbox. Lots of really ugly code, but it works!

--- ftp-client.lisp
+++ ftp-client.lisp
@@ -9,23 +9,66 @@
                                  :password "eUj8GeW55SvYaswqUyDSm5v6N")
     (ftp:send-nlst-command conn t)))
 
+(defvar *user* nil)
+(defvar *password* nil)
+(defvar *address* nil)
+(defvar *files-list* nil)
+
 (defun start-ui ()
-  (with-nodgui ()
-    (let* ((connect-button (make-instance 'button
-                                          :text    "Connect to server"
-                                          :command #'connect-to-server))
-           (address-entry (make-instance 'entry))
+  (with-nodgui (:title "FTP Client")
+    (let* ((address-entry (make-instance 'entry
+                                         :text "ftp.dlptest.com"))
            (address-label (make-instance 'label
                                          :text "FTP address:"))
+           (user-entry (make-instance 'entry
+                                      :text "dlpuser@dlptest.com"))
+           (user-label (make-instance 'label
+                                      :text "Username:"))
+           (password-entry (make-instance 'entry
+                                          :text "eUj8GeW55SvYaswqUyDSm5v6N"))
+           (password-label (make-instance 'label
+                                          :text "Password:"))
+           (files-label (make-instance 'label
+                                       :text "Files:"))
+           (files-list (make-instance 'listbox))
+           (connect-button (make-instance 'button
+                                          :text    "Connect to server"
+                                          :command (lambda ()
+                                                     (setf *user* (text user-entry))
+                                                     (setf *password* (text password-entry))
+                                                     (setf *address* (text address-entry))
+                                                     (connect-to-server))))
            )
-      (grid address-label 0 0)
-      (grid address-entry 0 1)
-      (grid connect-button 1 1)
+      (setf *files-list* files-list)
+      (grid address-label 0 0 :padx 5 :pady 5)
+      (grid address-entry 0 1 :padx 5 :pady 5)
+      (grid user-label 1 0 :padx 5 :pady 5)
+      (grid user-entry 1 1 :padx 5 :pady 5)
+      (grid password-label 2 0 :padx 5 :pady 5)
+      (grid password-entry 2 1 :padx 5 :pady 5)
+      (grid connect-button 3 1 :padx 5 :pady 5)
+      (grid files-label 4 0 :padx 5 :sticky :w)
+      (grid files-list 5 0 :padx 5 :pady 5 :columnspan 3 :sticky :we)
       (grid-columnconfigure *tk* :all :weight 1)
-      (grid-rowconfigure    *tk* :all :weight 1))))
-      )))
+      (grid-rowconfigure    *tk* :all :weight 1)
+)))
 
 (defun connect-to-server ()
-  (format *trace-output* "Called connect-to-server"))
+  (ftp:with-ftp-connection (conn :hostname *address*
+                                 :username *user*
+                                 :password *password*)
+    (listbox-clear *files-list*)
+    (format t "Cleared!~%")
+    ;; If I use #\newline, then it splits on \n only and leaves all \r
+    (let ((files (uiop:split-string (ftp:send-nlst-command conn nil)
+                                    :separator '(#\return #\linefeed))))
+      (mapcar (lambda (filename)
+                (unless (or (string= filename "")
+                            (string= filename ".")
+                            (string= filename ".."))
+                  (listbox-append *files-list* filename)))
+              files)))
+  (format t "Done!~%")
+  (finish-output))
 
 ;; (nodgui:with-nodgui () (nodgui:message-box (format nil "meh") "info" :ok "info"))
